614e03439932f8da3e6b5d7a4c4f443e755e5a37
Israel Sanchez <Israel.pro1994@gmail.com>
2021-10-20 16:52:21 -0500

    half of html is set need the other half then code the functions

---
+++ src/app/components/Shared/ingredient.model.ts
@@ -0,0 +1,16 @@
+export class Ingredient{
+    // public name: string;
+    // public amount: number;
+    
+    // constructor(n: string, a: number){
+    //     this.name = n;
+    //     this.amount = a;
+    // };   
+
+    // Either way works above or below!
+
+    constructor(public name: string, public amount: number){
+        
+    };
+
+};
