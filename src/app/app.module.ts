993a1355e0d43f6909524325911420da894aff20
Israel Sanchez <Israel.pro1994@gmail.com>
2021-10-20 10:25:11 -0500

    organizing folders and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { RecipesComponent } from './components/RecipeBook/recipes/recipes.component';
 import { RecipeListComponent } from './components/RecipeBook/recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './components/RecipeBook/recipes/recipe-list/recipe-item/recipe-item.component';
-
-import { IngredientComponent } from './components/ShoppingList/ingredient/ingredient.component';
+import { RecipeDetailComponent } from './components/RecipeBook/recipes/recipe-detail/recipe-detail.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ShoppingEditComponent } from './components/ShoppingList/shopping-list/shopping-edit/shopping-edit.component';
-import { RecipesComponent } from './components/RecipeBook/recipes/recipes.component';
+import { ShoppingListComponent } from './components/ShoppingList/shopping-list/shopping-list';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RecipesComponent,
     RecipeListComponent,
     RecipeItemComponent,
-    IngredientComponent,
+    RecipeDetailComponent,
     HeaderComponent,
     ShoppingEditComponent,
-    RecipesComponent
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule,
