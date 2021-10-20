3819f521172bb7a73c260dd46d295131fcd0548b
Israel Sanchez <Israel.pro1994@gmail.com>
2021-10-20 10:14:47 -0500

    organizing and renaming folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RecipeBookComponent } from './components/RecipeBook/recipe-book/recipe-book.component';
-import { RecipeListComponent } from './components/RecipeBook/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './components/RecipeBook/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './components/RecipeBook/recipe-detail/recipe-detail.component';
-import { RecipeComponent } from './components/RecipeBook/recipe/recipe.component';
-import { ShoppingListEditComponent } from './components/ShoppingList/shopping-list-edit/shopping-list-edit.component';
+
+import { RecipeListComponent } from './components/RecipeBook/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/RecipeBook/recipes/recipe-list/recipe-item/recipe-item.component';
+
 import { IngredientComponent } from './components/ShoppingList/ingredient/ingredient.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ShoppingEditComponent } from './components/ShoppingList/shopping-list/shopping-edit/shopping-edit.component';
+import { RecipesComponent } from './components/RecipeBook/recipes/recipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipeBookComponent,
     RecipeListComponent,
     RecipeItemComponent,
-    RecipeDetailComponent,
-    RecipeComponent,
-    ShoppingListEditComponent,
-    IngredientComponent
+    IngredientComponent,
+    HeaderComponent,
+    ShoppingEditComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
