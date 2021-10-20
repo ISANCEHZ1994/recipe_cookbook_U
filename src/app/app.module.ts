30a141c76a663c5065f67d518ce0a862cc0f7078
Israel Sanchez <Israel.pro1994@gmail.com>
2021-10-20 09:22:12 -0500

    adding recipe book components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeBookComponent } from './components/RecipeBook/recipe-book/recipe-book.component';
+import { RecipeListComponent } from './components/RecipeBook/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/RecipeBook/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './components/RecipeBook/recipe-detail/recipe-detail.component';
+import { RecipeComponent } from './components/RecipeBook/recipe/recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
