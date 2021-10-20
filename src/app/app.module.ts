83849bac9fc957baf490b227f7a10d2774c465e7
Israel Sanchez <Israel.pro1994@gmail.com>
2021-10-20 09:24:42 -0500

    adding shopping list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { RecipeListComponent } from './components/RecipeBook/recipe-list/recipe-
 import { RecipeItemComponent } from './components/RecipeBook/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './components/RecipeBook/recipe-detail/recipe-detail.component';
 import { RecipeComponent } from './components/RecipeBook/recipe/recipe.component';
+import { ShoppingListEditComponent } from './components/ShoppingList/shopping-list-edit/shopping-list-edit.component';
+import { IngredientComponent } from './components/ShoppingList/ingredient/ingredient.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { RecipeComponent } from './components/RecipeBook/recipe/recipe.component
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    RecipeComponent
+    RecipeComponent,
+    ShoppingListEditComponent,
+    IngredientComponent
   ],
   imports: [
     BrowserModule,
