9a3c5c4cbaecc96e4dbb20ed161a3c503426f490
Israel Sanchez <Israel.pro1994@gmail.com>
2021-12-01 15:37:52 -0600

    back to working on this course

--- src/app/components/ShoppingList/shopping-list/shopping-list.component.ts
+++ src/app/components/ShoppingList/shopping-list/shopping-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../../Shared/ingredient.model';
 
-
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
@@ -9,11 +8,14 @@ import { Ingredient } from '../../Shared/ingredient.model';
 })
 export class ShoppingListComponent implements OnInit {
 
-  ingredients: Ingredient[] []; 
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ]; 
 
-  constructor() { }
+  constructor() { };
 
   ngOnInit(): void {
-  }
+  };
 
-}
+};
