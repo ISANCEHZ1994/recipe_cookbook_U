ddc017daaba4ba60e3c4f357ff41c29712d37bde
Israel Sanchez <Israel.pro1994@gmail.com>
2022-02-28 09:51:40 -0600

    learning how to add an ingredient

--- src/app/components/ShoppingList/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/components/ShoppingList/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/components/Shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  // we pass in the name of the local reference..look inside the HTML file for the INPUT
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+
+  // NOTE: that the object passed thru is the same as model - so we replace to make it simplier
+  // OLD: <{ name: string, amount: number }>
+  // NOTE: it is already an object so only need the reference
+  ingredientAdded = new EventEmitter< Ingredient >();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddItem(){
+    const ingredientName = this.nameInputRef.nativeElement.value; // obtaining information from input
+    const ingredientAmount = this.amountInputRef.nativeElement.value; 
+    const newIngredient = new Ingredient(ingredientName, ingredientAmount); // using information to create NEW ingredients
   }
 
 }
