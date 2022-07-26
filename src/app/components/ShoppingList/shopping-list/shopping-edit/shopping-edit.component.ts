28592ae4652d870eb885f1df187b83f7995d92af
Israel Sanchez <Israel.pro1994@gmail.com>
2022-07-26 11:42:22 -0500

    Form changed to Template Driven in shopping-edit

--- src/app/components/ShoppingList/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/components/ShoppingList/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Ingredient } from 'app/components/Shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -8,8 +9,9 @@ import { ShoppingListService } from '../shopping-list.service';
 })
 export class ShoppingEditComponent implements OnInit {
   // we pass in the name of the local reference..look inside the HTML file for the INPUT
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef; 
+  // UPDATE: we are now switching to TD forms! thus we no longer need below!
+  // @ViewChild('nameInput') nameInputRef: ElementRef;
+  // @ViewChild('amountInput') amountInputRef: ElementRef; 
 
   // NOTE: that the object passed thru is the same as model - so we replace to make it simplier
   // OLD: <{ name: string, amount: number }>
@@ -22,11 +24,14 @@ export class ShoppingEditComponent implements OnInit {
   ngOnInit(): void {
   };
 
-  onAddItem(){
-    const ingredientName = this.nameInputRef.nativeElement.value; // obtaining information from input
-    const ingredientAmount = this.amountInputRef.nativeElement.value; 
-    const newIngredient = new Ingredient(ingredientName, ingredientAmount); // using information to create NEW ingredients
-    this.slService.addingIngredient( newIngredient );
+  onAddItem(form: NgForm){
+    // const ingredientName   = this.nameInputRef.nativeElement.value; // obtaining information from input
+    // const ingredientAmount = this.amountInputRef.nativeElement.value; 
+    // const newIngredient    = new Ingredient(ingredientName, ingredientAmount); // using information to create NEW ingredients
+    const value = form.value;
+    // we are getting the value from HTML file - switching to TD forms so know we make it simpiler
+    const newIngredient = new Ingredient( value.name, value.amount ); 
+    this.slService.addingIngredient( newIngredient ); 
   };
 
 };
