855ff64bff60c5f74d59297a24ca3bdc6baef95f
Israel Sanchez <Israel.pro1994@gmail.com>
2022-07-27 15:41:32 -0500

    clear button works

--- src/app/components/ShoppingList/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/components/ShoppingList/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -55,7 +55,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       this.subscription.unsubscribe();
   };
 
-  onAddItem(form: NgForm){
+  onSubmit(form: NgForm){ 
     // const ingredientName   = this.nameInputRef.nativeElement.value; // obtaining information from input
     // const ingredientAmount = this.amountInputRef.nativeElement.value; 
     // const newIngredient    = new Ingredient(ingredientName, ingredientAmount); // using information to create NEW ingredients
@@ -69,7 +69,13 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.slService.addIngredient( newIngredient );
     }
-    
+    this.editMode = false;
+    form.reset(); 
+  };
+
+  onClear(){
+    this.slForm.reset();
+    this.editMode = false;    
   };
 
 };
