41f7e3a54af2725b6b84fe53cb1c7a4e5b4faf3d
Israel Sanchez <Israel.pro1994@gmail.com>
2022-08-01 16:05:29 -0500

    validating user input

--- src/app/components/RecipeBook/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/components/RecipeBook/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
@@ -57,31 +57,41 @@ export class RecipeEditComponent implements OnInit {
         for( let ingredient of recipe.ingredients ){ // we want to use them - loop thru them
           recipeIngredients.push(                    // push them all into the default array
             new FormGroup({
-              'name'   : new FormControl( ingredient.name ),
-              'amount' : new FormControl( ingredient.amount )
+              // added Validators
+              'name'  : new FormControl( ingredient.name, Validators.required ),
+              'amount': new FormControl( ingredient.amount, [
+                    Validators.required,
+                    Validators.pattern(/^[1-9]+[0-9]*$/)
+              ])
             })
-          )
+          );
         }
       }
     };
 
     this.recipeForm = new FormGroup({
       // the string should match the formControlName parameter of the inputs inside of the HTML
-      'name'        : new FormControl(recipeName),
-      'imagePath'   : new FormControl(recipeImagePath),
-      'description' : new FormControl(recipeDescription),
-      'ingredients' : recipeIngredients // should be everything we just pushed to the default array
+      // note: we are also adding Validators to the mix to make sure we have the right datatype
+      'name'       : new FormControl(recipeName, Validators.required),
+      'imagePath'  : new FormControl(recipeImagePath, Validators.required),
+      'description': new FormControl(recipeDescription, Validators.required),
+      'ingredients': recipeIngredients // should be everything we just pushed to the default array
     });
   }; // initForm CLOSE  
 
   onAddIngredient(){
     // we are explicityly casting it - basically enclosing the type we want to convert
-    (<FormArray>this.recipeForm.get('ingredients')).push(
+    (<FormArray>this.recipeForm.get('ingredients'))
+    .push(
       new FormGroup({
-        'name'  : new FormControl(),
-        'amount': new FormControl()
+        // added Validators
+        'name'  : new FormControl( null, Validators.required ),
+        'amount': new FormControl( null, [
+              Validators.required,
+              Validators.pattern(/^[1-9]+[0-9]*$/)
+        ])
       })
-    )
+    );
   };
 
   get controls() { // a getter!
