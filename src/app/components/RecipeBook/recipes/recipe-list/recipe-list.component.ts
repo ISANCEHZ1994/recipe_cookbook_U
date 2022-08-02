cc8120b6bc4d9a8b9c1b451628c73943fd588951
Israel Sanchez <Israel.pro1994@gmail.com>
2022-08-02 09:50:40 -0500

    Can now edit and create new recipes

--- src/app/components/RecipeBook/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/RecipeBook/recipes/recipe-list/recipe-list.component.ts
@@ -21,7 +21,9 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit(): void {
     this.recipeService.recipesChanged.subscribe(
-      
+      ( recipes: Recipe[] ) => {
+        this.recipes = recipes;
+      }
     );
     this.recipes = this.recipeService.getRecipies();
   };
