887aaac13d8826c365c24cf2d9d5d76615063db1
Israel Sanchez <Israel.pro1994@gmail.com>
2022-08-12 16:00:04 -0500

    resolver class created

--- src/app/components/RecipeBook/recipes/recipes-resolver.service.ts
+++ src/app/components/RecipeBook/recipes/recipes-resolver.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
 import { DataStorageService } from "app/components/Shared/data-storage.service";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 
 import { Recipe } from "./recipe.model";
+import { RecipeService } from "./recipe.service";
 
 @Injectable({
     providedIn: 'root'
 })
 // Resolve <generic> interface used
-export class RecipesResolverService implements Resolve<Recipe[]> { // again defining which type of data it will resolve
+export class RecipesResolverService implements Resolve<Recipe[]> { // again defining which type of data it will resolve    
 
-    constructor( private dataSS: DataStorageService ){};
+    constructor( private dataSS: DataStorageService, private recipeService: RecipeService ){};
 
     resolve( route: ActivatedRouteSnapshot, state: RouterStateSnapshot ){
-        return this.dataSS.fetchRecipes();
+        const recipes = this.recipeService.getRecipes();
+
+        if( RecipeListComponent.length === 0 ){
+            return this.dataSS.fetchRecipes();
+        } else {
+            return recipes;
+        }
+        
     };
 };
