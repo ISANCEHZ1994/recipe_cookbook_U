7e61df69824566b643a7c87a98784f3bda0ff042
Israel Sanchez <Israel.pro1994@gmail.com>
2022-08-22 16:47:10 -0500

    moving to recipes routing - organizing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,21 @@
 import { NgModule }     from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { RecipeDetailComponent }  from "./components/RecipeBook/recipes/recipe-detail/recipe-detail.component";
-import { RecipeEditComponent }    from "./components/RecipeBook/recipes/recipe-edit/recipe-edit.component";
-import { RecipeStartComponent }   from "./components/RecipeBook/recipes/recipe-start/recipe-start.component";
-import { RecipesResolverService } from "./components/RecipeBook/recipes/recipes-resolver.service";
-import { RecipesComponent }       from "./components/RecipeBook/recipes/recipes.component";
+
 import { ShoppingListComponent }  from "./components/ShoppingList/shopping-list/shopping-list.component";
-import { AuthComponent }          from "./auth/auth.component";
-import { AuthGuard }              from "./auth/auth-guard";
+import { AuthComponent } from "./auth/auth.component";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'recipes', 
-      canActivate: [AuthGuard], 
-      component: RecipesComponent, 
-      children: [
-        { path: '',          component: RecipeStartComponent }, 
-        { path: 'new',       component: RecipeEditComponent }, 
-        { path: ':id',       component: RecipeDetailComponent, resolve: [ RecipesResolverService ] }, 
-        { path: ':id/edit',  component: RecipeEditComponent } 
-    ]},    
+    // MOVED TO RECIPES-ROUTING.MODULE
+    // { path: 'recipes', 
+    //   canActivate: [AuthGuard], 
+    //   component: RecipesComponent, 
+    //   children: [
+    //     { path: '',          component: RecipeStartComponent }, 
+    //     { path: 'new',       component: RecipeEditComponent }, 
+    //     { path: ':id',       component: RecipeDetailComponent, resolve: [ RecipesResolverService ] }, 
+    //     { path: ':id/edit',  component: RecipeEditComponent } 
+    // ]},    
     { path: 'shopping-list', component: ShoppingListComponent },
     { path: 'auth',          component: AuthComponent }
 ];
--- src/app/components/RecipeBook/recipes/recipes.module.ts
+++ src/app/components/RecipeBook/recipes/recipes.module.ts
@@ -5,14 +5,19 @@
 // the components that are going to be moved here have all have something to do with recipe(s) - which is why it is also in this folder (DUH)
 
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+
 import { RecipesComponent } from "./recipes.component";
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component"; 
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipesRoutingModule } from "./recipes-routing.module";
 
-@NgModule({    
+@NgModule({ 
     declarations: [
         RecipesComponent,
         RecipeListComponent,
@@ -33,7 +38,16 @@ import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
         RecipeStartComponent,
         RecipeEditComponent
     ],
-        
+    // so if we check our router-outlet inside of recipes.component.html - there is an error 
+    // the router only works for the app.module.ts so we must also use it inside of the imports
+    imports: [
+        RouterModule,
+        // there were more erros appearing 
+        ReactiveFormsModule,
+        CommonModule,
+        // we have moved everything that has to do with the recipes routes into this new file
+        RecipesRoutingModule
+    ]    
 })
 export class RecipesModule {
 
