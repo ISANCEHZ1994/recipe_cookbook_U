b02ff72637d45484ac5fb09676d7df8bc4cfc4fe
Israel Sanchez <Israel.pro1994@gmail.com>
2022-10-03 22:37:14 -0500

    adding reducer notes - similar to redux set up

--- src/app/components/ShoppingList/shopping-list/shopping-list.reducer.ts
+++ src/app/components/ShoppingList/shopping-list/shopping-list.reducer.ts
@@ -3,6 +3,7 @@
 // we will need to use something to help mangage the state better
 
 // our service - shopping-list.service.ts will be replaced with this file!
+import { Action } from "@ngrx/store";
 import { Ingredient } from "app/components/Shared/ingredient.model";
 
 const initalState = {
@@ -12,8 +13,28 @@ const initalState = {
     ]
 };
 
-export function shoppingListReducer( state = initalState, action ){
-    CASE: ''
-    DEFAULT: ''
+export function shoppingListReducer( state = initalState, action: Action ){
+
+    // possibly for later! 
+    // const { type, payload } = action;
+
+    switch( action.type ){
+        case 'ADD_INGREDIENT': 
+            // below would be completely WRONG! state changes with NgRx always have to be IMMUTABLE
+            // meaning we can not edit the existing or previous state
+            // ===> return state.ingreidents.push()
+            // INSTEAD return a new object to replace the state
+            // COPY the old state with spread operator (like in REDUX)
+            return {
+                ...state, // techinally you don't have to add it because there is only one item in the state but REALLY good practice!
+                ingredients: [ // remember - the item we are changing is the ingredients array
+                    ...state.ingredients,
+                    action  
+                ]
+            };
+        default:
+            return state;
+    }
+   
 };
 
